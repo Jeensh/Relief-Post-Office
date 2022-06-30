43f0f1ef6ff92918a6273d5ae7714ce0d87b66d1
jeensh <jeensh25@gmail.com>
2022-06-30 15:43:05 +0900

    init project

---
+++ Relief_Post_Office/app/src/main/java/com/Seoul42/relief_post_office/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.Seoul42.relief_post_office
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
