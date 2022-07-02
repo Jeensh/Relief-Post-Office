7a8e1f016d973370a547075869e7347315adcaf1
moonheee <enduser0@naver.com>
2022-07-03 00:16:21 +0900

    (FEAT:result) textWardName

--- Relief_Post_Office/app/src/main/java/com/Seoul42/relief_post_office/ResultActivity.kt
+++ Relief_Post_Office/app/src/main/java/com/Seoul42/relief_post_office/ResultActivity.kt
@@ -5,16 +5,29 @@ import android.os.Bundle
 import android.util.Log
 import com.Seoul42.relief_post_office.databinding.ActivityResultBinding
 import com.bumptech.glide.Glide
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 
 class ResultActivity : AppCompatActivity() {
     val binding by lazy { ActivityResultBinding.inflate(layoutInflater) }
+    val database = Firebase.database
     val storage = Firebase.storage("gs://relief-post-office-58784.appspot.com")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        downloadImage("/profile/userid-1.png")
+        //intent로 넘어와야 할 정보들
+        val wardId = "userid-5"
+        //끝
+        database.getReference("users")
+            .child(wardId)
+            .child("name")
+            .get()
+            .addOnSuccessListener {
+            binding.textWardName.text = it.value.toString()
+        }
+        downloadImage("/profile/${wardId}.png")
         setContentView(binding.root)
+
     }
     fun downloadImage(path: String) {
         storage.getReference(path).downloadUrl.addOnSuccessListener { uri ->
